1789f0e77e43ba8becc1a43132ecdaf66f5913f1
Raghu Betina <raghu@firstdraft.com>
2019-03-19 19:08:34 +0000

    Whitelist columns in LiftTotalDetail for ActiveAdmin

--- app/admin/lift_total_details.rb
+++ app/admin/lift_total_details.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register LiftTotalDetail do
+
+ permit_params :lift_total_id, :squat, :bench, :deadlift
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
