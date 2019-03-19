bdd0c65cfcdb7bcfd3d9a80060a63cd6e05598ce
Raghu Betina <raghu@firstdraft.com>
2019-03-19 19:08:54 +0000

    Only profile user can modify profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,14 @@
 class ProfilesController < ApplicationController
+  before_action :current_user_must_be_profile_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_profile_user
+    profile = Profile.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == profile.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @profiles = Profile.all
 
