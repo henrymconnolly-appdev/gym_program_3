9bb8700d971273431110b12a72584ebe71b125bc
Raghu Betina <raghu@firstdraft.com>
2019-03-19 19:08:53 +0000

    Automatically associate profile to signed in user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -42,7 +42,7 @@ class ProfilesController < ApplicationController
   def update_row
     @profile = Profile.find(params.fetch("id_to_modify"))
 
-    @profile.user_id = params.fetch("user_id")
+    
 
     if @profile.valid?
       @profile.save
