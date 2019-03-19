2f9deee6d954fcc99cabea6868f1d3ef48eae7e6
Raghu Betina <raghu@firstdraft.com>
2019-03-19 19:08:31 +0000

    Whitelist columns in Feed for ActiveAdmin

--- app/admin/feeds.rb
+++ app/admin/feeds.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Feed do
+
+ permit_params :user_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
